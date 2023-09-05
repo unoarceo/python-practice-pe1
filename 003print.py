da5256188b1f8633cfc146654d7ca5a2d139d1ab
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-05 14:16:52 +0800

    print function

---
+++ 003print.py
@@ -0,0 +1,39 @@
+# One argument
+print("One argument!")
+print()
+
+# Escape character
+print("The quick brown fox\nis just very quick.")
+print()
+
+# 2 print lines
+print("First line.")
+print("Second line.")
+print()
+
+# Two or more arguments
+print("Hello,", "Arjay!")
+print()
+
+# Keyword argument = end
+print("My name is", end=" ")
+print("Arjay.")
+print()
+
+# Keyword argument = end = \n
+print("My name is", end="\n")
+print("Arjay.")
+print()
+
+# Keyword argument = end = rand
+print("My name is", end="mariah carey , ")
+print("Arjay.")
+print()
+
+# Keyword argument = sep
+print("Hello", "Arjay!","Are","you","a","beginner?",sep="*")
+print()
+
+# Keyword argument - MIXED
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
