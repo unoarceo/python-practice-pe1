a1236c134ff03f4efd57a5d4579f4be25a0f7c80
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-06 14:27:09 +0800

    Samples for literals

---
+++ 004literals.py
@@ -0,0 +1,50 @@
+# integer
+print(5)
+print()
+
+#octal
+print(0o123)
+print()
+
+#hexa
+print(0x123)
+print()
+
+#float-normal
+print(5.0)
+print()
+
+#float-exponential
+print(3e8)
+print()
+
+#string-escape character
+print("I love \"Monty Python\"")
+print('I\'m Monty Python')
+print()
+
+#string-single/double quote
+print('I love "Monty Python"')
+print("I'm Monty Python")
+print()
+
+#boolean
+print(True > False)
+print(True < False)
+print()
+
+#lab
+'''
+    Scenario:
+    Write a one-line piece of code, 
+    using the print() function, 
+    as well as the newline and escape characters, 
+    to match the expected result outputted on three lines.
+
+    Expected Output:
+    "I'm"
+    ""learning""
+    """Python"""
+'''
+print("LAB:")
+print('"I\'m"\n""learning""\n"""Python"""')
