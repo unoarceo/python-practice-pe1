f00227fbd05ce3dcfac64bd54d280e7372808980
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-12 12:11:05 +0800

    update input()

--- 008input.py
+++ 008input.py
@@ -1,3 +1,28 @@
+
 x = input("Enter your name: ")
 print(x)
 print()
+
+x = input()
+y = input()
+print(x+y)
+print()
+
+
+
+x = int(input())
+y = int(input())
+x = x // y
+y = y // x
+print(y)
+
+print(1//2*3)
+
+x = int(input())
+y = int(input())
+x = x%y
+print(x,y)
+x = x%y
+print(x,y)
+y = y%x
+print(x,y)
