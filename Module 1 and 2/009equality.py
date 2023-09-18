2f83130048970db11b8f8aaf4f043267f2a80104
Arjay Arceo <arjay.m.arceo@gmail.com>
2023-09-18 13:35:57 +0800

    equality , comparison, and conditional

---
+++ Module 1 and 2/009equality.py
@@ -0,0 +1,11 @@
+var = 0  # Assigning 0 to var
+print(var == 0)
+
+var = 1  # Assigning 1 to var
+print(var == 0)
+
+var = 0  # Assigning 0 to var
+print(var != 0)
+
+var = 1  # Assigning 1 to var
+print(var != 0)
---
+++ Module 1 and 2/010comparison.py
@@ -0,0 +1,10 @@
+"""
+Using one of the comparison operators in Python, 
+write a simple two-line program that takes the parameter n as input, 
+which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+
+Don't create any if blocks (we're going to talk about them very soon). 
+"""
+
+n = int(input())
+print(n>=100)
